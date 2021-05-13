5b52012b1edbb373860b133c0ffc4d62d56aecbc
Shungz <shungjiu89@gmail.com>
2021-05-14 02:58:22 +0800

    Exercise 3

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
@@ -1,6 +1,8 @@
 package com.techyourchance.multithreading.exercises.exercise3;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,9 @@ public class Exercise3Fragment extends BaseFragment {
 
     private Button mBtnCountSeconds;
     private TextView mTxtCount;
+
+
+    private final Handler mUiHandler = new Handler(Looper.getMainLooper());
 
     @Nullable
     @Override
@@ -55,5 +60,46 @@ public class Exercise3Fragment extends BaseFragment {
         3. Show count in TextView
         4. When count completes, show "done" in TextView and enable the button
          */
+
+        //1. Disable button to prevent multiple clicks
+
+        mBtnCountSeconds.setEnabled(false);
+        //2. Start counting on background thread using loop and Thread.sleep()
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= SECONDS_TO_COUNT; i++) {
+                    final int count = i;
+                    //mUiHandler is doing task at main thread
+                    mUiHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTxtCount.setText(String.valueOf(count));
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+//                3. Show count in TextView
+//                4. When count completes, show "done" in TextView and enable the button
+
+                //After done for loop, is time to bring back to UiThread
+                //mUiHandler is doing task at main thread
+                mUiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTxtCount.setText("Done!");
+                        mBtnCountSeconds.setEnabled(true);
+                    }
+                });
+            }
+        }).start();
+
+
+
     }
 }
