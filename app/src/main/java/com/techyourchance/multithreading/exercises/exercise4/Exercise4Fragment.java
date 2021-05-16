e7e8a120011b1e168f384626df8f97dc87bb05a8
Shungz <shungjiu89@gmail.com>
2021-05-16 13:57:35 +0800

    Attended Exercise 4

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise4/Exercise4Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise4/Exercise4Fragment.java
@@ -17,6 +17,7 @@ import com.techyourchance.multithreading.R;
 import com.techyourchance.multithreading.common.BaseFragment;
 
 import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,21 +32,22 @@ public class Exercise4Fragment extends BaseFragment {
 
     private static int MAX_TIMEOUT_MS = DefaultConfiguration.DEFAULT_FACTORIAL_TIMEOUT_MS;
 
-    private Handler mUiHandler = new Handler(Looper.getMainLooper());
+    private final Handler mUiHandler = new Handler(Looper.getMainLooper());
 
+    // UI thread
     private EditText mEdtArgument;
     private EditText mEdtTimeout;
     private Button mBtnStartWork;
     private TextView mTxtResult;
 
-    private int mNumberOfThreads;
-    private ComputationRange[] mThreadsComputationRanges;
-    private BigInteger[] mThreadsComputationResults;
-    private int mNumOfFinishedThreads;
+    private int mNumberOfThreads; // safe
+    private ComputationRange[] mThreadsComputationRanges; // safe
+    private volatile BigInteger[] mThreadsComputationResults; // safe
+    private final AtomicInteger mNumOfFinishedThreads = new AtomicInteger(0); // safe
 
-    private long mComputationTimeoutTime;
+    private long mComputationTimeoutTime; // safe
 
-    private boolean mAbortComputation;
+    private volatile boolean mAbortComputation; // safe
 
     @Nullable
     @Override
@@ -121,7 +123,7 @@ public class Exercise4Fragment extends BaseFragment {
         mNumberOfThreads = factorialArgument < 20
                 ? 1 : Runtime.getRuntime().availableProcessors();
 
-        mNumOfFinishedThreads = 0;
+        mNumOfFinishedThreads.set(0);
 
         mAbortComputation = false;
 
@@ -169,7 +171,7 @@ public class Exercise4Fragment extends BaseFragment {
                         product = product.multiply(new BigInteger(String.valueOf(num)));
                     }
                     mThreadsComputationResults[threadIndex] = product;
-                    mNumOfFinishedThreads++;
+                    mNumOfFinishedThreads.incrementAndGet();
                 }
             }).start();
 
@@ -179,7 +181,7 @@ public class Exercise4Fragment extends BaseFragment {
     @WorkerThread
     private void waitForThreadsResultsOrTimeoutOrAbort() {
         while (true) {
-            if (mNumOfFinishedThreads == mNumberOfThreads) {
+            if (mNumOfFinishedThreads.get() == mNumberOfThreads) {
                 break;
             } else if(mAbortComputation) {
                 break;
