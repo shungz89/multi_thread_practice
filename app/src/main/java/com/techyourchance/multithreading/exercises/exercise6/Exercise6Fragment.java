c1cf739cd77cefe3b7f1afe6cadc63e5d6abd7f8
Shungz <shungjiu89@gmail.com>
2021-05-16 22:53:57 +0800

    Attended Exercise 6

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise6/Exercise6Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise6/Exercise6Fragment.java
@@ -2,8 +2,6 @@ package com.techyourchance.multithreading.exercises.exercise6;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,14 +15,12 @@ import com.techyourchance.multithreading.R;
 import com.techyourchance.multithreading.common.BaseFragment;
 
 import java.math.BigInteger;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.WorkerThread;
 import androidx.fragment.app.Fragment;
 
-public class Exercise6Fragment extends BaseFragment {
+public class Exercise6Fragment extends BaseFragment implements ComputeFactorialUseCase.Listener {
 
     public static Fragment newInstance() {
         return new Exercise6Fragment();
@@ -32,24 +28,18 @@ public class Exercise6Fragment extends BaseFragment {
 
     private static int MAX_TIMEOUT_MS = DefaultConfiguration.DEFAULT_FACTORIAL_TIMEOUT_MS;
 
-    private final Object LOCK = new Object();
-
-    private final Handler mUiHandler = new Handler(Looper.getMainLooper());
-
-    // UI thread
     private EditText mEdtArgument;
     private EditText mEdtTimeout;
     private Button mBtnStartWork;
     private TextView mTxtResult;
 
-    private int mNumberOfThreads; // safe
-    private ComputationRange[] mThreadsComputationRanges; // safe
-    private volatile BigInteger[] mThreadsComputationResults; // safe
-    private int mNumOfFinishedThreads = 0; // safe
+    private ComputeFactorialUseCase mComputeFactorialUseCase;
 
-    private long mComputationTimeoutTime; // safe
-
-        private volatile boolean mAbortComputation; // safe
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mComputeFactorialUseCase = new ComputeFactorialUseCase();
+    }
 
     @Nullable
     @Override
@@ -76,16 +66,23 @@ public class Exercise6Fragment extends BaseFragment {
 
             int argument = Integer.valueOf(mEdtArgument.getText().toString());
 
-            computeFactorial(argument, getTimeout());
+            mComputeFactorialUseCase.computeFactorialAndNotify(argument, getTimeout());
         });
 
         return view;
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        mComputeFactorialUseCase.registerListener(this);
+    }
+
+    @Override
     public void onStop() {
         super.onStop();
-        mAbortComputation = true;
+        mComputeFactorialUseCase.unregisterListener(this);
+
     }
 
     @Override
@@ -106,144 +103,21 @@ public class Exercise6Fragment extends BaseFragment {
         return timeout;
     }
 
-    private void computeFactorial(final int factorialArgument, final int timeout) {
-        new Thread(() -> {
-            initComputationParams(factorialArgument, timeout);
-            startComputation();
-            waitForThreadsResultsOrTimeoutOrAbort();
-            processComputationResults();
-        }).start();
+    @Override
+    public void onFactorialComputed(BigInteger result) {
+        mTxtResult.setText(result.toString());
+        mBtnStartWork.setEnabled(true);
     }
 
-    private void initComputationParams(int factorialArgument, int timeout) {
-        mNumberOfThreads = factorialArgument < 20
-                ? 1 : Runtime.getRuntime().availableProcessors();
-
-        synchronized (LOCK) {
-            mNumOfFinishedThreads = 0;
-        }
-
-        mAbortComputation = false;
-
-        mThreadsComputationResults = new BigInteger[mNumberOfThreads];
-
-        mThreadsComputationRanges = new ComputationRange[mNumberOfThreads];
-
-        initThreadsComputationRanges(factorialArgument);
-
-        mComputationTimeoutTime = System.currentTimeMillis() + timeout;
+    @Override
+    public void onFactorialComputationTimedOut() {
+        mTxtResult.setText("Computation timed out");
+        mBtnStartWork.setEnabled(true);
     }
 
-    private void initThreadsComputationRanges(int factorialArgument) {
-        int computationRangeSize = factorialArgument / mNumberOfThreads;
-
-        long nextComputationRangeEnd = factorialArgument;
-        for (int i = mNumberOfThreads - 1; i >= 0; i--) {
-            mThreadsComputationRanges[i] = new ComputationRange(
-                    nextComputationRangeEnd - computationRangeSize + 1,
-                    nextComputationRangeEnd
-            );
-            nextComputationRangeEnd = mThreadsComputationRanges[i].start - 1;
-        }
-
-        // add potentially "remaining" values to first thread's range
-        mThreadsComputationRanges[0].start = 1;
-    }
-
-    @WorkerThread
-    private void startComputation() {
-        for (int i = 0; i < mNumberOfThreads; i++) {
-
-            final int threadIndex = i;
-
-            new Thread(() -> {
-                long rangeStart = mThreadsComputationRanges[threadIndex].start;
-                long rangeEnd = mThreadsComputationRanges[threadIndex].end;
-                BigInteger product = new BigInteger("1");
-                for (long num = rangeStart; num <= rangeEnd; num++) {
-                    if (isTimedOut()) {
-                        break;
-                    }
-                    product = product.multiply(new BigInteger(String.valueOf(num)));
-                }
-                mThreadsComputationResults[threadIndex] = product;
-
-                synchronized (LOCK) {
-                    mNumOfFinishedThreads++;
-                    LOCK.notifyAll();
-                }
-
-            }).start();
-
-        }
-    }
-
-    @WorkerThread
-    private void waitForThreadsResultsOrTimeoutOrAbort() {
-        synchronized (LOCK) {
-            while (mNumOfFinishedThreads != mNumberOfThreads && !mAbortComputation && !isTimedOut()) {
-                try {
-                    LOCK.wait(getRemainingMillisToTimeout());
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-        }
-    }
-
-    private long getRemainingMillisToTimeout() {
-        return mComputationTimeoutTime - System.currentTimeMillis();
-    }
-
-    @WorkerThread
-    private void processComputationResults() {
-        String resultString;
-
-        if (mAbortComputation) {
-            resultString = "Computation aborted";
-        }
-        else {
-            resultString = computeFinalResult().toString();
-        }
-
-        // need to check for timeout after computation of the final result
-        if (isTimedOut()) {
-            resultString = "Computation timed out";
-        }
-
-        final String finalResultString = resultString;
-
-        mUiHandler.post(() -> {
-            if (!Exercise6Fragment.this.isStateSaved()) {
-                mTxtResult.setText(finalResultString);
-                mBtnStartWork.setEnabled(true);
-            }
-        });
-    }
-
-    @WorkerThread
-    private BigInteger computeFinalResult() {
-        BigInteger result = new BigInteger("1");
-        for (int i = 0; i < mNumberOfThreads; i++) {
-            if (isTimedOut()) {
-                break;
-            }
-            result = result.multiply(mThreadsComputationResults[i]);
-        }
-        return result;
-    }
-
-    private boolean isTimedOut() {
-        return System.currentTimeMillis() >= mComputationTimeoutTime;
-    }
-
-    private static class ComputationRange {
-        private long start;
-        private long end;
-
-        public ComputationRange(long start, long end) {
-            this.start = start;
-            this.end = end;
-        }
+    @Override
+    public void onFactorialComputationAborted() {
+        mTxtResult.setText("Computation aborted");
+        mBtnStartWork.setEnabled(true);
     }
 }
